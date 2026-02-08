210ded2f9105f31ffeb1d7e15c308a54a20bcb3d
erkinovamandarr-tech <erkinovamandarr@gmail.com>
2026-02-08 18:46:59 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,20 +3,18 @@ import TelegramBot from "node-telegram-bot-api";
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token);
 
-// Vercel webhook handler
 export default async function handler(req, res) {
   if (req.method === "POST") {
     bot.processUpdate(req.body);
     res.status(200).send("OK");
   } else {
-    res.status(200).send("Telegram Test Bot ishlayapti 🚀");
+    res.status(200).send("Bot ishlayapti 🚀");
   }
 }
 
-// /start komandasi
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(
     msg.chat.id,
-    "👋 Salom!\n\n📘 IELTS Test Botga xush kelibsiz!\n\nTez orada testlar qo‘shiladi 🔥"
+    "👋 Salom! Telegram Test Bot ishlayapti ✅"
   );
 });
